adbc381feddba3c5330b2f2c993c80cb181d5326
Simon Fetzel <simon@fec-a68fjbs9si.lan>
2014-02-02 20:12:15 +0100

    zusätzliche Methoden zur SimpleExerciseFactory hinzugefügt

--- www/js/lib/SimpleExerciseFactory.js
+++ www/js/lib/SimpleExerciseFactory.js
@@ -9,21 +9,30 @@ function SimpleExerciseFactory()
 
 util.inherits(SimpleExerciseFactory, SpecificExerciseFactory);
 
+SimpleExerciseFactory.prototype.getFirstNumber = function(numberLength)
+{
+	var firstNumber = this.getRandomNumber(Math.pow(10, numberLength-1), Math.pow(10, numberLength));
+	return firstNumber;
+}
+
+SimpleExerciseFactory.prototype.getSecondNumber = function(numberLength, firstNumber)
+{
+	return this.getFirstNumber(numberLength);
+}
+
 SimpleExerciseFactory.prototype.getRandomExercise = function(difficulty)
 {
 	if(difficulty == undefined || parseInt(difficulty) == Math.NaN)
 		throw "difficulty argument missing";
 	
 	var numberLength = this.getNumberLength(difficulty);
-
-	var firstNumber = this.getRandomNumber(Math.pow(10, numberLength-1), Math.pow(10, numberLength));
-	var secondNumber = this.getRandomNumber(Math.pow(10, numberLength-1), Math.pow(10, numberLength));
+	var firstNumber = this.getFirstNumber(numberLength);
+	var secondNumber = this.getSecondNumber(numberLength, firstNumber);
 
 	var correctAnswer = this.calculate(firstNumber, secondNumber);
 
 	var exercise = new Exercise(firstNumber + this.operationSymbol.toString() + secondNumber, correctAnswer);
 	return exercise;
 }
-
 
 module.exports = SimpleExerciseFactory;
---
+++ www/js/lib/SubtractionExerciseFactory.js
@@ -0,0 +1,18 @@
+var SimpleExerciseFactory = require("../SimpleExerciseFactory");
+var util = require("util");
+
+function SubtractionExerciseFactory()
+{
+	SimpleExerciseFactory.call(this);
+	this.operationSymbol = '-';
+}
+
+util.inherits(SubtractionExerciseFactory, SimpleExerciseFactory);
+
+SubtractionExerciseFactory.prototype.calculate = function(firstNumber, secondNumber)
+{
+	return firstNumber - secondNumber;
+}
+
+
+module.exports = SubtractionExerciseFactory;
